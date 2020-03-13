648cc99464cbda66c7c3cafd562365b50a2cc8d4
Gregg Garrison <gregg.garrison@rocketmail.com>
2020-03-13 16:37:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,7 +10,7 @@ end
 def add_element_to_end_of_array(array, element)
   array =  ["a", "b", "c", "d"]
   element = "e"
-  array.push("e")
+  add_element_to_end_of_array(array, element)array.push("e")
   
 end
 
